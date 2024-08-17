0350c5a0951e6198e9028cbcad022136442ad86c
Wasanjith <wasakodi79@gmail.com>
2024-08-17 14:31:02 +0530

    new file:   .env 	new file:   .gitignore 	new file:   backend/controllers/auth.controller.js 	new file:   backend/db/connectMongoDB.js 	new file:   backend/routes/auth.routes.js 	new file:   backend/server.js 	new file:   frontend/README.md 	new file:   frontend/eslint.config.js 	new file:   frontend/index.html 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/public/vite.svg 	new file:   frontend/src/App.css 	new file:   frontend/src/App.jsx 	new file:   frontend/src/assets/react.svg 	new file:   frontend/src/index.css 	new file:   frontend/src/main.jsx 	new file:   frontend/vite.config.js 	new file:   package-lock.json 	new file:   package.json
    Connecting succsessfully to MongoDB

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+import { connect } from "mongoose";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+const app = express();
+const PORT =process.env.PORT || 5000;
+
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the server!");
+});
+
+app.use("/api/auth",authRoutes);
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
+});
